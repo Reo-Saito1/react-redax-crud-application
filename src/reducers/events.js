f92a543f8fc4af4d3bd6f75320780693dd7396f4
齋藤礼欧 <saitoreo@saitouayaounoMacBook-Pro.local>
2022-06-13 23:50:08 +0900

    implement delete feature

--- src/reducers/events.js
+++ src/reducers/events.js
@@ -1,11 +1,17 @@
 import _ from "lodash"
-import {READ_EVENTS} from "../actions/index"
+import {
+    READ_EVENTS,
+    DELETE_EVENT
+} from "../actions/index"
 
-export default (events = {},action) => {
-    switch (action.type){
+export default (events = {}, action) => {
+    switch (action.type) {
         case READ_EVENTS:
-            console.log(_.mapKeys(action.response.data,'id'))
-            return _.mapKeys(action.response.data,'id')
+            console.log(_.mapKeys(action.response.data, 'id'))
+            return _.mapKeys(action.response.data, 'id')
+        case DELETE_EVENT:
+            delete events[action.id]
+            return { ...events }
         default:
             return events
     }
